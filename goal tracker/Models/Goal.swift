1b2453a5b99bf33177887278e5f1fc729e4b063b
Henrik Persson <henrik.perssn@gmail.com>
2025-03-02 11:44:23 +0100

    Created Goal and habit models with relationship

--- goal tracker/Models/Goal.swift
+++ goal tracker/Models/Goal.swift
@@ -4,9 +4,21 @@
 //
 //  Created by Henrik Persson on 2025-03-02.
 //
-
+import SwiftData
 import Foundation
 
-struct Goal {
-
+@Model
+final class Goal {
+    var type: String
+    var targetValue: Double
+    var timeFrame: TimeInterval
+    var createdDate: Date
+    @Relationship(deleteRule: .cascade) var habits: [Habit]?
+    
+    init(type: String, targetValue: Double, timeFrame: TimeInterval) {
+        self.type = type
+        self.targetValue = targetValue
+        self.timeFrame = timeFrame
+        self.createdDate = Date()
+    }
 }
--- goal tracker/Models/Habit.swift
+++ goal tracker/Models/Habit.swift
@@ -4,5 +4,20 @@
 //
 //  Created by Henrik Persson on 2025-03-02.
 //
-
+import SwiftData
 import Foundation
+
+@Model
+final class Habit {
+    var date: Date
+    var targetValue: Double
+    var isCompleted: Bool
+    var actualValue: Double?
+    @Relationship(deleteRule: .cascade, inverse: \Goal.habits) var goal: Goal?
+    
+    init(date: Date, targetValue: Double) {
+        self.date = date
+        self.targetValue = targetValue
+        self.isCompleted = false
+    }
+}
