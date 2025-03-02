7238d85b108ee027becd7efefe8ababbdc7b2234
Henrik Persson <henrik.perssn@gmail.com>
2025-03-02 11:32:21 +0100

    Init file structure

---
+++ goal tracker/Models/Goal.swift
@@ -0,0 +1,12 @@
+//
+//  Goal.swift
+//  goal tracker
+//
+//  Created by Henrik Persson on 2025-03-02.
+//
+
+import Foundation
+
+struct Goal {
+
+}
---
+++ goal tracker/Models/Habit.swift
@@ -0,0 +1,8 @@
+//
+//  Habit.swift
+//  goal tracker
+//
+//  Created by Henrik Persson on 2025-03-02.
+//
+
+import Foundation
